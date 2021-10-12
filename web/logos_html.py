ee41c8c1602d81d736934b30cbc15c2d6e1dcb7f
deathbybandaid <github@deathbybandaid.net>
2021-10-12 11:38:08 -0400

    Implement Listing

--- web/logos_html.py
+++ web/logos_html.py
@@ -1,6 +1,9 @@
 from flask import request, render_template_string, session
+import os
 import pathlib
 from io import StringIO
+
+import urllib.parse
 
 
 class Logos_html():
@@ -13,6 +16,8 @@ class Logos_html():
         self.fhdhr = fhdhr
         self.plugin_utils = plugin_utils
 
+        self.logo_files = pathlib.Path(plugin_utils.path).joinpath('logos')
+
         self.template_file = pathlib.Path(plugin_utils.path).joinpath('logos.html')
         self.template = StringIO()
         self.template.write(open(self.template_file).read())
@@ -22,4 +27,16 @@ class Logos_html():
 
     def get(self, *args):
 
-        return render_template_string(self.template.getvalue(), request=request, session=session, fhdhr=self.fhdhr, list=list, origin=self.plugin_utils.namespace)
+        logo_list = self.list_logos
+        logo_dicts = []
+        for logo in logo_list:
+            logo_dicts.append({
+                "name": logo.split(".")[0],
+                "url": "/api/logos?logo_name=%s" % urllib.parse.quote(logo.split(".")[0])
+            })
+
+        return render_template_string(self.template.getvalue(), request=request, session=session, fhdhr=self.fhdhr, logo_dicts=logo_dicts, list=list, origin=self.plugin_utils.namespace)
+
+    @property
+    def list_logos(self):
+        return os.listdir(self.logo_files)
